bbfafddfbc78251d824052d9ac1e2da7e199ab39
zerOne123 <528541096@qq.com>
2022-11-06 16:22:50 +0800

    test

--- api/user.py
+++ api/user.py
@@ -1,19 +1,19 @@
-# from flask import Flask, Response
-# app = Flask(__name__)
+from flask import Flask, Response
+app = Flask(__name__)
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
-from http.server import BaseHTTPRequestHandler
+# from http.server import BaseHTTPRequestHandler
  
  
-class handler(BaseHTTPRequestHandler):
+# class handler(BaseHTTPRequestHandler):
  
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write("DearXuan's API by python!".encode())
-        return
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.send_header('Content-type', 'text/plain')
+#         self.end_headers()
+#         self.wfile.write("DearXuan's API by python!".encode())
+#         return
