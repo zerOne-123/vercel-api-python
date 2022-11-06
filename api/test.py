60d6b6a600120f2bdf23c9e0f8d6d42154f38710
zerOne123 <528541096@qq.com>
2022-11-06 21:46:18 +0800

    test

--- api/test.py
+++ api/test.py
@@ -2,7 +2,6 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': 'pp'})
 @app.route('/<path:path>')
 def test(path):
   name = request.args.get('name', 'py')
