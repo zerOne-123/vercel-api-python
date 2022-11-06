7564751ddbe0144618cb889108daaa141cabced5
zerOne123 <528541096@qq.com>
2022-11-06 20:07:17 +0800

    test

--- api/index.py
+++ api/index.py
@@ -5,7 +5,6 @@ import base64
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
   return 'Hello, world'
@@ -19,8 +18,6 @@ def madou():
   key = 'pnhXgN0U'
   iv = 'GY4gEvBD'
   url = request.args.get("url")
-
   img = requests.get(url).content
   des = DES.new(key=key.encode(), mode=DES.MODE_CBC, iv=iv.encode())
   return base64.b64decode(des.decrypt(base64.b64decode(img)).decode().replace('data:image/jpg;base64,', ''))
-  # return img
