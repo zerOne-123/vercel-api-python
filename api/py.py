7f8f3f6cf6736d8a561e8b3aedfb18d717acda41
zerOne123 <528541096@qq.com>
2022-11-06 17:12:28 +0800

    main

--- api/py.py
+++ api/py.py
@@ -2,10 +2,10 @@ from flask import Flask, request, Response, make_response
 import requests
 app = Flask(__name__)
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 
 @app.route("/madou",methods=["GET"])
 def madou():
