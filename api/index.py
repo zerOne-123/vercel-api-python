f2c3c1919c7885970d4d9d471fa9db3df576b9e1
zerOne123 <528541096@qq.com>
2022-11-06 19:54:00 +0800

    test

--- api/index.py
+++ api/index.py
@@ -1,13 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from Crypto.Cipher import AES, DES
+import requests
+import base64
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, world'
-
+  return 'Hello, world'
 
 @app.route('/test')
 def test():
-    return 'Test'
+  return 'Test'
+
+@app.route('/madou')
+def madou():
+  key = 'pnhXgN0U'
+  iv = 'GY4gEvBD'
+  url = request.args.get("url")
+
+  img = requests.get(url).content
+  des = DES.new(key=key.encode(), mode=DES.MODE_CBC, iv=iv.encode())
+  return base64.b64decode(des.decrypt(base64.b64decode(img)).decode().replace('data:image/jpg;base64,', ''))
+  # return img
--- api/test.py
+++
@@ -1,9 +0,0 @@
-from sanic import Sanic
-from sanic.response import json
-app = Sanic(name='api')
-
-@app.route('/')
-@app.route('/<path:path>')
-async def index(request, path=""):
-  # print(request)
-  return json({'hello': path})
