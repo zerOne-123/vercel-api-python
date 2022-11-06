e4dab653d45ab7fbde2b512dece4e99dea803e82
zerOne123 <528541096@qq.com>
2022-11-06 17:00:35 +0800

    test

--- api/py.py
+++ api/py.py
@@ -1,10 +1,25 @@
-from flask import Flask, Response
+from flask import Flask, request, Response, make_response
+import requests
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+
+@app.route("/madou",methods=["GET"])
+def img():
+    key = 'pnhXgN0U'
+    iv = 'GY4gEvBD'
+    url = request.args.get("url")
+    
+    if url == '':
+        return ""
+    
+    img = requests.get(url).content
+    # des = DES.new(key=key.encode(), mode=DES.MODE_CBC, iv=iv.encode())
+    # return des.decrypt(pad(img, 16, style='pkcs7'))
+    return img
 
 # from http.server import BaseHTTPRequestHandler
  
