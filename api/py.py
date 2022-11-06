0ade99615a16a819d5b3727b03f51d979e944401
zerOne123 <528541096@qq.com>
2022-11-06 16:32:13 +0800

    test

--- api/py.py
+++ api/py.py
@@ -1,10 +1,9 @@
 from flask import Flask, Response
 app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+@app.route('/')
+def catch_all():
+    return 'aaa'
 
 # from http.server import BaseHTTPRequestHandler
  
