d4d83682dd1597c0c04dab5262407494b2665a67
zerOne123 <528541096@qq.com>
2022-11-07 23:05:22 +0800

    test

--- api/test.py
+++ api/test.py
@@ -4,5 +4,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def test():
-  name = request.args.get('name', 'pky')
+  name = request.args.get('name', 'py')
+  return '/ ' + name
+
+@app.route('/api/test')
+def test():
+  name = request.args.get('name', 'py')
+  return '/api/test ' + name
+
+@app.route('/api/test/mm')
+def test():
+  name = request.args.get('name', 'py')
+  return '/api/test/mm ' + name
+
+@app.route('/api/test/m')
+def test():
+  name = request.args.get('name', 'py')
+  return '/api/test/m ' + name
+
+@app.route('/<path:path>')
+def test(path):
+  name = request.args.get('name', 'py')
   return 'Hello ' + name
