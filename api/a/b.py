027e1918e608c532db5aee2a5d12f58653c67289
zerOne123 <528541096@qq.com>
2022-11-08 22:02:04 +0800

    test

--- api/a/b.py
+++ api/a/b.py
@@ -3,6 +3,6 @@ from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/<path:path>')
-def test5(path):
+def test5():
   name = request.args.get('name', 'py')
   return 'Hello a b ' + name
