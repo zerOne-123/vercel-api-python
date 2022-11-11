7dae4999a3de2b1582e70c8dcf789cfbb6ef8136
zerOne123 <528541096@qq.com>
2022-11-11 22:14:21 +0800

    test

--- api/js.js
+++ api/js.js
@@ -1,7 +1,9 @@
-const axios = require('axios').default
+const axios = require('axios').default;
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   const { name = 'js' } = req.query;
-  const html = (await axios.get('https://www.baidu.com')).data
+  const html = await axios.get('https://www.baidu.com').then((res) => {
+    return res.data;
+  });
   return res.send(html);
-}
+};
