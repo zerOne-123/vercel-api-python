ebaea4fca7f61bbfa7a806d706299f880ab538f4
zerOne123 <528541096@qq.com>
2022-11-20 22:45:26 +0800

    test

--- api/a.py
+++ api/a.py
@@ -1,9 +1,10 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import HTTPResponse, text
+from sanic.request import Request
 
 app = Sanic('vercel-api-python')
 
 
 @app.route('/<path:path>')
-async def index(request, path=""):
-    return text("Hello, world.")
+async def index(request: Request, path='') -> HTTPResponse:
+    return text("Done.")
