a9b6bbc87aaa9ca9de5a4faad70586bec231b766
zerOne123 <528541096@qq.com>
2022-11-20 22:38:13 +0800

    test

--- api/a.py
+++ api/a.py
@@ -1,8 +1,9 @@
 from sanic import Sanic
-from sanic.response import json
+from sanic.response import text
+
 app = Sanic('vercel-api-python')
 
 
 @app.route('/<path:path>')
 async def index(request, path=""):
-    return json({'hello': path})
+    return text("Hello, world.")
