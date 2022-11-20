723776f82ba8dac4c4c24aac598c8aa799c6d7cd
zerOne123 <528541096@qq.com>
2022-11-20 23:21:59 +0800

    test

--- api/a.py
+++ api/a.py
@@ -2,7 +2,7 @@ from sanic import Sanic, response
 from sanic.response import HTTPResponse, text
 from sanic.request import Request
 
-app = Sanic('api')
+app = Sanic(__name__)
 
 @app.get("/")
 @app.route('/<path:path>')
