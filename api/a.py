91c60acd893ba5b2f1887ba03c92c3a27dfe3ce9
zerOne123 <528541096@qq.com>
2022-11-22 22:34:15 +0800

    test

--- api/a.py
+++ api/a.py
@@ -2,13 +2,10 @@ from sanic import Sanic, response
 from sanic.response import HTTPResponse, text
 from sanic.request import Request
 
-app = Sanic('api')
+app = Sanic('a')
 
 @app.get("/")
 @app.route('/<path:path>')
 async def index(request: Request, path='') -> HTTPResponse:
     # return text("Done.")
     return response.text('hello sanic.')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8888)
