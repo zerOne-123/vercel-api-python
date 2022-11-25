246c8b42361e14d9f3c83b3ea7544c1b00e1addb
zerOne123 <528541096@qq.com>
2022-11-25 22:29:17 +0800

    test

---
+++ api/fastapi.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/{path:path}')
+async def root(path):
+    return {"message": "Hello World", "path": path}
--- api/flask.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/<path:path>')
-def catch_all(path):
-    return {"message": "Hello World", "path": path}
